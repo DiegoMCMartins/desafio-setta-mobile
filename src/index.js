dbab81d3aec956e9fe4dcca7e4404a9985525b7c
DiegoMCMartins <diegomcastro2.8@gmail.com>
2021-06-08 19:51:26 -0300

    chore: add condition to return Playground for tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import Playground from './Playground';
+
+const isOnTest = true;
+
 export default function App() {
+  if (isOnTest) {
+    return <Playground />;
+  }
+
   return (
     <View>
       <Text>Ola</Text>
