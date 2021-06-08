97dbca7888bcf36db32fd5ebc0e54dfe617126ed
DiegoMCMartins <diegomcastro2.8@gmail.com>
2021-06-08 11:01:43 -0300

    chore: add playground file for tests

---
+++ src/Playground.js
@@ -0,0 +1,14 @@
+/*
+  This file is used to test some components.
+*/
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Playground() {
+  return (
+    <View>
+      <Text>Playground</Text>
+    </View>
+  );
+}
